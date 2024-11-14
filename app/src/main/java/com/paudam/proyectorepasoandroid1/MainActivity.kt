3bb00e3d3e38e95d91c031f73b50c84d5becbfce
paupc <p.pulido.c@institutvidreres.cat>
2024-11-14 08:47:33 +0100

    Spinner base done

--- app/src/main/java/com/paudam/proyectorepasoandroid1/MainActivity.kt
+++ app/src/main/java/com/paudam/proyectorepasoandroid1/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.paudam.proyectorepasoandroid1
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.paudam.proyectorepasoandroid1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val KEY_CURS = "curs"
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        val spinner: Spinner = findViewById(R.id.spinner)
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.cursos,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/paudam/proyectorepasoandroid1/PageDataUsers.kt
@@ -0,0 +1,5 @@
+package com.paudam.proyectorepasoandroid1
+
+class PageDataUsers {
+
+}
